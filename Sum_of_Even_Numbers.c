91de3836fbc5d463849e11ac059d5c0945546066
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-08-30 08:56:19 +0530

    New Commit

---
+++ Sum_of_Even_Numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter A Number\n");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        if(i%2 == 0)
+            sum += i;
+    }
+
+    printf("Sum of Even Numbers are: %d", sum);
+    return 0;
+}
