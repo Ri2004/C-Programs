7b9c9b1811b1a598b3b7f67123931535a3e278ad
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-08-31 20:22:36 +0530

    New Commit

---
+++ Fibonacci_series_upto_a_certain_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void fibo(int n)
+{
+    int prev_num, current_num, next_num;
+    prev_num = 0;
+    current_num = 1;
+    printf("%d, %d, ", prev_num, current_num);
+
+    next_num = prev_num + current_num;
+    printf("%d, ", next_num);
+
+    while (next_num <= n)
+    {
+        printf("%d, ", next_num);
+        prev_num = current_num;
+        current_num = next_num;
+        next_num = prev_num + current_num;
+    }
+}
+int main()
+{
+    int num;
+    printf("Enter A Number\n");
+    scanf("%d", &num);
+
+    fibo(num);
+    return 0;
+}
