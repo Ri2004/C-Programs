4f36719db52a94889f62210fe022a04c32112c76
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-09-01 08:22:53 +0530

    Modified

--- Fibonacci_series.c
+++ Fibonacci_series.c
@@ -4,13 +4,13 @@ void fibo(int n)
     int prev_num, current_num, next_num;
     prev_num = 0;
     current_num = 1;
-    printf("%d, %d, ",prev_num, current_num);
+    printf("%d %d, ",prev_num, current_num);
 
     next_num = prev_num + current_num;
 
     while (current_num<n)
     {
-        printf("%d, ", next_num);
+        printf("%d ", next_num);
         prev_num = current_num;
         current_num = next_num;
         next_num = prev_num + current_num;
