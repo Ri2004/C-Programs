80fda2eb91348106dd4d2a3147b9133cbb3d8950
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-09-02 08:21:21 +0530

    New Commit

---
+++ Armstrong_Number.c
@@ -0,0 +1,31 @@
+// Armstrong Number
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int num, temp, rem, i, length = 0, sum = 0;
+    printf("Enter A Number\n");
+    scanf("%d", &num);
+
+    temp = num;
+    for (; temp > 0;)
+    {
+        length++;
+        temp /= 10;
+    }
+    temp = num;
+    while(temp>0)
+    {
+        rem = temp % 10;
+        sum = sum + (pow(rem, length));
+        temp = temp / 10;
+    }
+
+    if(num == sum)
+        printf("%d is an Armstrong Number", num);
+
+    else
+        printf("%d is not an Armstrong Number", num); 
+
+    return 0;
+}
