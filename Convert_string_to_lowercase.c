a4028421534807a0cebb37a25c8e734c98ebb4ca
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-09-07 19:48:05 +0530

    New Commit

---
+++ Convert_string_to_lowercase.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int length = 0, i;
+    char s[200];
+
+    printf("Enter A String\n");
+    gets(s);
+
+    for (i = 0; s[i] != '\0';  i++)
+    {
+        length++;
+    }
+
+    for (i = 0; i < length; i++)
+    {
+        if (s[i] >= 65 && s[i] <= 90)
+        {
+            s[i] = s[i] + 32;
+        }
+    }
+    printf("After convert into lowercase the string is %s\n", s);
+    return 0;
+}
