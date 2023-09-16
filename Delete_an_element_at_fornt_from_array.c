7bc39ebc03da8fd1c2c8c9ca4a9b2a560d408001
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-09-16 20:43:36 +0530

    Modified

--- Delete_an_element_at_fornt_from_array.c
+++ Delete_an_element_at_fornt_from_array.c
@@ -7,29 +7,37 @@ int main()
     printf("Enter size of array\n");
     scanf("%d", &n);
 
-    printf("Enter array elements\n");
-    for (i = 0; i < n; i++)
+    if (n > 50)
     {
-        scanf("%d", &arr[i]);
+        printf("Overflow Condition.\n");
     }
+    else
+    {
+        printf("Enter array elements\n");
+        for (i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
 
-    printf("Elements of the array is\n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
+        printf("Elements of the array is\n");
+        for (i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
 
-    num = arr[0];
-    for (j = 0; j < n; j++)
-    {
-        arr[j] = arr[j + 1];
-    }
+        num = arr[0];
+        for (j = 0; j < n; j++)
+        {
+            arr[j] = arr[j + 1];
+        }
 
-    printf("\nAfter delete the %d at front from array\n", num);
-    n--;
-    for (j = 0; j < n; j++)
-    {
-        printf("%d ", arr[j]);
+        printf("\nAfter delete the %d at front from array\n", num);
+        n--;
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", arr[j]);
+        }
     }
+        
     return 0;
 }
