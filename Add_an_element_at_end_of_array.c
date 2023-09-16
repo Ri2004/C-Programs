85ed49052daa637a0347555c8b66798ee17078e4
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-09-16 10:10:55 +0530

    New commit

---
+++ Add_an_element_at_end_of_array.c
@@ -0,0 +1,36 @@
+// Add an element at end in array
+// for insert an element in array we must create an array which is not full i.e. number of elements must be less than the size of array
+
+#include<stdio.h>
+int main()
+{
+    int arr[50], n, i, num;
+    printf("Enter Size of array\n");
+    scanf("%d", &n);
+
+    printf("Enter Array Elements\n");
+    for (i = 0; i < (n-5); i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Elements of array is\n");
+    for (i = 0; i < (n - 5); i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+
+    for (i = 0; arr[i] != 0; i++);
+
+    printf("Enter element which you want to insert at end\n");
+    scanf("%d", &num);
+
+    arr[i] = num;
+    printf("After inserting %d in Array the array is\n", num);
+
+    for (int j = 0; j <= i; j++)
+    {
+        printf("%d ", arr[j]);
+    }
+    return 0;
+}
