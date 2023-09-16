9068c8ef70a20f9120298f7260381c7c08d73234
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-09-16 19:40:30 +0530

    New Commit

---
+++ Delete_an_element_at_fornt_from_array.c
@@ -0,0 +1,37 @@
+// Delete an element from front of the array
+
+#include<stdio.h>
+int main()
+{
+    int arr[50] = {0}, n, i, j, num;
+    printf("Enter size of array\n");
+    scanf("%d", &n);
+
+    printf("Enter array elements\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Elements of the array is\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    num = arr[0];
+    for (j = 0; j < n; j++)
+    {
+        arr[j] = arr[j + 1];
+    }
+
+    for (i = 0; arr[i] != 0; i++);
+
+    printf("\nAfter delete the %d at front from array\n", num);
+
+    for (j = 0; j < i; j++)
+    {
+        printf("%d ", arr[j]);
+    }
+    return 0;
+}
