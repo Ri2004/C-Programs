040ec3e86807d492ef168f576564da81fdcad364
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-10-30 12:53:15 +0530

    Modified

--- Linked_list_implementation.c
+++ Linked_list_implementation.c
@@ -14,27 +14,21 @@ int main()
     struct node *newnode, *temp = NULL;
     int n = 1;
 
-    while(n!=0)
+    while(n)
     {
-        printf("Enter choice if you want to create a node\n");
+        printf("Enter choice between 0 and 1, 0 for not want to create any node and 1 for create another node\n");
         scanf("%d", &n);
 
         if (n == 0)
-        {
-            temp->link = NULL;
             break;
-        }
-            
         else
         {
             newnode = (struct node *)malloc(sizeof(struct node));
             printf("Enter the data\n");
             scanf("%d", &newnode->data);
-
+            newnode->link = NULL;
             if (head == NULL)
-            {
                 head = temp = newnode;
-            }
             else
             {
                 temp->link = newnode;
