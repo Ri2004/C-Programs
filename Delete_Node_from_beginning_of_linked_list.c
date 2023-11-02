e8e9f16b3dcd86a2288c769386c7bceee66bdfc4
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-11-02 21:03:25 +0530

    New Commit

---
+++ Delete_Node_from_beginning_of_linked_list.c
@@ -0,0 +1,67 @@
+// Delete Node from beginning of Linked List 
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+struct node *head = NULL, *temp = NULL, *newnode = NULL;
+
+void create_linked_list();
+void display_list();
+void delete_node_from_beginning_of_linked_list();
+
+int main()
+{
+    create_linked_list();
+    display_list();
+    delete_node_from_beginning_of_linked_list();
+    display_list();
+    return 0;
+}
+
+void create_linked_list()
+{
+    int choice = 1;
+    while(choice)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("Enter data of node\n");
+        scanf("%d", &newnode->data);
+        newnode->link = NULL;
+
+        if (head == NULL)
+        {
+            head = temp = newnode;
+        }
+        else
+        {
+            temp->link = newnode;
+            temp = temp->link;
+        }
+
+        printf("Do You add another node in Linked List\n");
+        scanf("%d", &choice);
+    }
+}
+
+void display_list()
+{
+    temp = head;
+    printf("\nAfter insert data in Linked List the elements are\n");
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->link;
+    }
+}
+
+void delete_node_from_beginning_of_linked_list()
+{
+    struct node *delete_node = head;
+    head = head->link;
+    free(delete_node);
+    delete_node = NULL;
+}
