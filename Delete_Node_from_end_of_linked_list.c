f3b4780f6121a83040071321f8d70700a6e87fa2
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-11-02 21:20:21 +0530

    New commit

--- Delete_Node_from_end_of_linked_list.c
+++ Delete_Node_from_end_of_linked_list.c
@@ -1,4 +1,4 @@
-// Delete Node from beginning of Linked List 
+// Delete Node from end of Linked List 
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -11,13 +11,13 @@ struct node *head = NULL, *temp = NULL, *newnode = NULL;
 
 void create_linked_list();
 void display_list();
-void delete_node_from_beginning_of_linked_list();
+void delete_node_from_end_of_linked_list();
 
 int main()
 {
     create_linked_list();
     display_list();
-    delete_node_from_beginning_of_linked_list();
+    delete_node_from_end_of_linked_list();
     display_list();
     return 0;
 }
@@ -32,17 +32,14 @@ void create_linked_list()
         scanf("%d", &newnode->data);
         newnode->link = NULL;
 
-        if (head == NULL)
-        {
+        if(head == NULL)
             head = temp = newnode;
-        }
         else
         {
             temp->link = newnode;
             temp = temp->link;
         }
-
-        printf("Do You add another node in Linked List\n");
+        printf("Do you add another node in Linked List, if yes press 1 else 0\n");
         scanf("%d", &choice);
     }
 }
@@ -50,7 +47,7 @@ void create_linked_list()
 void display_list()
 {
     temp = head;
-    printf("\nAfter insert data in Linked List the elements are\n");
+    printf("\nAfter Insert data in Linked List the elements of Linked List are\n");
     while (temp != NULL)
     {
         printf("%d ", temp->data);
@@ -58,15 +55,22 @@ void display_list()
     }
 }
 
-void delete_node_from_beginning_of_linked_list()
+void delete_node_from_end_of_linked_list()
 {
-    if (head == NULL)
+    struct node *end = NULL, *prevnode = NULL;
+
+    if(head == NULL)
         printf("No Nodes are there. Linked List is empty.");
     else
     {
-        struct node *delete_node = head;
-        head = head->link;
-        free(delete_node);
-        delete_node = NULL;
-    }    
+        end = head;
+        while (end->link != NULL)
+        {
+            prevnode = end;
+            end = end->link;
+        }
+    }
+    prevnode->link = NULL;
+    free(end);
+    end = NULL;
 }
